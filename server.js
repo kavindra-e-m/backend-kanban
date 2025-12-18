8062df52fad929050c4b8ee94c26a68a84490cd0
Kavindra E M <kavindraem@gmail.com>
2025-12-18 12:11:03 +0530

    Add Vercel frontend URL to CORS allowed origins

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
+  "https://mern-frontend-rjfc.vercel.app",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
